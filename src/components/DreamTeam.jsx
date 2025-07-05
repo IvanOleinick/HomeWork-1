e43e08db76b643cf45a8617ab673ffff9d3dca7b
ivanOleinick <oleinick.eminemkod31317@gmail.com>
2025-07-06 00:41:47 +0300

    Add custom classes to specific friend images

--- src/components/DreamTeam.jsx
+++ src/components/DreamTeam.jsx
@@ -5,8 +5,14 @@ const DreamTeam = () => {
     return (
         <section className="float-end row w-50 border rounded-bottom-4 me-0 ms-2">
             <h2 className="text-center">Dream team</h2>
-            {arrayImages.map((item ,index)=> <Friends friend={item} key={index}/>)}
+            {arrayImages.map((item, index) => {
+                let extraClass = "";
 
+                if (index === 6) extraClass = "bottomLeft";
+                if (index === 8) extraClass = "bottomRight";
+
+                return <Friends friend={item} className={extraClass} key={index} />;
+            })}
         </section>
     );
 };
--- src/components/Friends.jsx
+++ src/components/Friends.jsx
@@ -1,6 +1,6 @@
-const Friends = ({friend}) => {
+const Friends = ({friend,className}) => {
     return (
-        <img className="col-sm-4 p-1" src={friend} alt="Friend"/>
+        <img className={`col-sm-4 p-1 ${className}`} src={friend} alt="Friend" />
 
 
     );
